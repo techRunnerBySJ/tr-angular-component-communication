2d087e9bec127d79f2cbae1d19d9373bacee39e5
salonij20 <saloni.j@speedyy.com>
2023-06-27 17:58:31 +0530

    refactor: app module updated

--- tr-angular-component-communication/src/app/app.module.ts
+++ tr-angular-component-communication/src/app/app.module.ts
@@ -1,12 +1,16 @@
+import { ParentComponent } from './../components/parent/parent.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ChildComponent } from 'src/components/child/child.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ParentComponent,
+    ChildComponent
   ],
   imports: [
     BrowserModule,
